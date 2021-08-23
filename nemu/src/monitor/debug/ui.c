ff06c4863136437f9f504a4dc20c14aa8233cb9d
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-23 22:08:03 +0800

    >  compile NEMU 3020244259 mme Linux mme-superpower 5.4.0-66-generic #74~18.04.2-Ubuntu SMP Fri Feb 5 11:17:31 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:08:03 up  2:08,  1 user,  load average: 0.38, 0.22, 0.15 4183d38a60817e473722b6a6801ce1b83e6023a9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -43,11 +43,11 @@ static int cmd_x(char *args) {
 	int length;
 	sscanf(add,"%x",&address);
 	sscanf(N,"%d",&length);
-printf("%0#8x\n",address);
+printf("%08x\n",address);
 	int t;
 	for(t=0;t<length;t++)
 	{
-      printf("%0#8x\n",swaddr_read(address,4));
+      printf("%08x\n",swaddr_read(address,4));
 	  address+=4;
 	}
 	return 0;
