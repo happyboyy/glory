70186ea3e694254ed0e259002202716622a448ff
tracer-NEMU2020 <tjuics2020@163.com>
2021-08-28 21:46:38 +0800

    >  compile NEMU 3020244259 mme Linux mme-superpower 5.4.0-66-generic #74~18.04.2-Ubuntu SMP Fri Feb 5 11:17:31 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:46:38 up  1:09,  1 user,  load average: 0.80, 0.39, 0.28 e5411a61eb4a0213df7379a36c84f8a751db29a2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -257,7 +257,7 @@ uint32_t eval(int p, int q, bool *success){
 			}
 		}
 		int op = mo(p,q);
-		printf("%d",op);
+		printf("5");
 		int value1 = eval(p,op - 1,success);
 		int value2 = eval(op + 1,q,success);
 		int op_type = tokens[op].type;
